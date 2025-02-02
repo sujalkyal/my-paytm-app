4ad38525d6f0dfc758e351d2ee58b8d9332626f9
sujalkyal <sujalkyal2704@gmail.com>
2025-02-02 11:34:22 +0530

    UI and on ramping done

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,13 @@
-import db from "@repo/db/client";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "../lib/auth";
 
-export default function Home() {
-  return (
-    <div className="text-4xs">
-      hi there !!
-    </div>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+  
 }
